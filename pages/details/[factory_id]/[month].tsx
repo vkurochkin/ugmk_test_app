72bf919641b894adde905747c89f45114183f519
vkurochkin <kurochkin@firecode-team.ru>
2023-07-14 17:26:51 +0300

    remove ssr from details page due to docker issues

--- pages/details/[factory_id]/[month].tsx
+++ pages/details/[factory_id]/[month].tsx
@@ -1,6 +1,5 @@
 import {useRouter} from "next/router";
 import {Pie, PieChart, Tooltip} from "recharts";
-import {GetServerSideProps} from "next";
 import {useEffect, useState} from "react";
 
 import {IProduct} from "@/common/interfaces/Product";
@@ -13,17 +12,36 @@ interface IMonthProduct {
     value: number;
 }
 
-export default function Page({data}: any) {
-    const [productsAreLoaded, setProductsAreLoaded] = useState<boolean>(false);
+export default function Page() {
+    const [products, setProducts] = useState<Array<IMonthProduct>>([]);
+    const { query } = useRouter();
 
     useEffect(() => {
-        setProductsAreLoaded(true);
-    }, []);
+        (async () => {
+            const res = await fetch('http://localhost:3001/products');
+            const data = await res.json();
 
-    const { query } = useRouter();
+            const { factory_id, month } = query;
+            const result: Array<IMonthProduct> = productTypes.filter(productType => productType.id > 0)
+                .map(productType => ({name: productType.value, value: 0}));
+
+            if (factory_id && month) {
+                data.filter((product: IProduct) =>
+                    product.factory_id === +factory_id && getMonthFromDateStr(product.date) === +month)
+                    .forEach((elem: IProduct) => {
+                        result[0].value += elem.product1;
+                        result[1].value += elem.product2;
+                        result[2].value += elem.product3;
+                    });
+            }
+
+            setProducts(result);
+        })()
+    }, [query]);
+
     const { factory_id, month } = query;
 
-    return (productsAreLoaded &&
+    return (
         <div className={styles.main}>
             {factory_id && month
                 && <h2>производство продукции на Фабрике {+factory_id === 1 ? 'A' : 'Б'} в {months[+month]}</h2>
@@ -32,7 +50,7 @@ export default function Page({data}: any) {
                 <Pie
                     dataKey="value"
                     isAnimationActive={false}
-                    data={data}
+                    data={products}
                     cx="50%"
                     cy="50%"
                     outerRadius={100}
@@ -43,27 +61,4 @@ export default function Page({data}: any) {
             </PieChart>
         </div>
     );
-}
-
-export const getServerSideProps: GetServerSideProps<{
-    data: Array<IMonthProduct>
-}> = async ({ query }) => {
-    const res = await fetch('http://localhost:3001/products');
-    const data = await res.json();
-
-    const { factory_id, month } = query;
-    const result: Array<IMonthProduct> = productTypes.filter(productType => productType.id > 0)
-        .map(productType => ({name: productType.value, value: 0}));
-
-    if (factory_id && month) {
-        data.filter((product: IProduct) =>
-            product.factory_id === +factory_id && getMonthFromDateStr(product.date) === +month)
-            .forEach((elem: IProduct) => {
-                result[0].value += elem.product1;
-                result[1].value += elem.product2;
-                result[2].value += elem.product3;
-            });
-    }
-
-    return { props: { data: result } }
 }
